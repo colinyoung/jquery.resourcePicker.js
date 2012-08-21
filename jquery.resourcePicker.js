f484088bfa49c9df6a74492bcb60d0b79e9b8c84
Colin Young <me@colinyoung.com>
2012-08-20 22:49:55 -0500

    First commit

---
+++ jquery.resourcePicker.js
@@ -0,0 +1,31 @@
+/*
+Simple jQuery plugin that redirects the UA to a resource URL onChange.
+
+Ex:
+$('#mySelectTag').resourcePicker()
+
+<select data-resource="post">
+  <option value="1">Post 1</option>
+</select>
+*/
+
+$.fn.resourcePicker = function(options) {
+  var _this = this;
+  
+  this.resource = $(this).attr('data-resource');
+  if (typeof this.resource === "undefined") {
+    console.debug("jQuery.resourcePicker.js: You must add a `data-resource' attribute to your select tag.");
+    return;
+  }
+  
+  $(this).change(function(e) {
+    var selected = $(this).find('option:selected');
+    _this.redirectTo(selected);
+  });
+  
+  this.redirectTo = function(selected) {
+    var value = selected.val(),
+        name = selected.text();
+    location.href = '/' + this.resource.pluralize() + '/' + value;
+  };
+};
